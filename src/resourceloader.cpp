bfdfb4c0a21d937620695a3a173f8cfb3895c7a3
Brandon Montell <brandonmontell@gmail.com>
2015-04-04 13:13:32 -0400

    Made it easier to see shader compile errors

--- src/resourceloader.cpp
+++ src/resourceloader.cpp
@@ -107,14 +107,17 @@ GLuint ResourceLoader::createProgramFromShaders(std::vector<GLuint> &shaders)
     glLinkProgram(programId);
 
     // Print the info log.
-    GLint result = GL_FALSE;
-    int infoLogLength;
+    GLint result;
+    GLint infoLogLength;
     glGetProgramiv(programId, GL_LINK_STATUS, &result);
     glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &infoLogLength);
     if (infoLogLength > 0) {
         std::vector<char> infoLog( std::max(infoLogLength, int(1)) );
         glGetProgramInfoLog(programId, infoLogLength, NULL, &infoLog[0]);
         fprintf(stdout, "%s\n", &infoLog[0]);
+
+        // Exit if shader program not linked.
+        if (result == GL_FALSE) exit(1);
     }
 
     return programId;
@@ -140,14 +143,17 @@ GLuint ResourceLoader::createShader(GLenum shaderType, const char *filepath)
     glCompileShader(shaderID);
 
     // Print info log.
-    GLint result = GL_FALSE;
-    int infoLogLength;
+    GLint result;
+    GLint infoLogLength;
     glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
     glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
     if (infoLogLength > 0) {
         std::vector<char> infoLog(infoLogLength);
         glGetShaderInfoLog(shaderID, infoLogLength, NULL, &infoLog[0]);
         fprintf(stdout, "%s\n", &infoLog[0]);
+
+        // Exit if shader not compiled.
+        if (result == GL_FALSE) exit(1);
     }
 
     return shaderID;
